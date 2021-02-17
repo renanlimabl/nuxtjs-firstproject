11e2ae1dfb1892cb145cbc9302762487c9cfaff5
Renan Lima <renanlimabl@gmail.com>
2021-02-17 15:48:33 -0300

    add navbar component

---
+++ components/TheNavbar.vue
@@ -0,0 +1,14 @@
+<template>
+  <nav class="nav">
+    <div class="logo">
+      <a href="#" class="logo text-lg">Nuxt Fundamentals</a>
+      <span class="subheader">A Vue School course</span>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
+    <the-navbar />
     <Nuxt />
   </div>
 </template>
+
+<script>
+import TheNavbar from '@/components/TheNavbar'
+
+export default {
+  components: {
+    TheNavbar
+  }
+}
+</script>
