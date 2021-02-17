a87c798c72906e70eb8ffe9548fa8899b7675b73
Renan Lima <renanlimabl@gmail.com>
2021-02-17 16:29:14 -0300

    add linking between pages

--- pages/posts/_id.vue
+++ pages/posts/_id.vue
@@ -6,6 +6,19 @@
       </h1>
       <p>{{ post.content }}</p>
     </article>
+    <aside>
+      <h3>Post you might enjoy</h3>
+      <ul>
+        <li v-for="related in relatedPosts" :key="related">
+          <!-- <nuxt-link :to="`/posts/${related.id}`">
+            {{ related.title }}
+          </nuxt-link> -->
+          <nuxt-link :to="{name: 'posts-id', params: {id: related.id}}">
+            {{ related.title }}
+          </nuxt-link>
+        </li>
+      </ul>
+    </aside>
   </div>
 </template>
 
@@ -36,6 +49,10 @@ export default {
   computed: {
     post () {
       return this.posts.find(post => post.id === this.id)
+    },
+    // filter the posts id where i'm :D
+    relatedPosts () {
+      return this.posts.filter(post => post.id !== this.id)
     }
   }
 }
